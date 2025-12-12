174f9eab0ab27aa66f78a5b59df2ec4ffd023c2e
ana2546 <aromero582@alumnos.iua.edu.ar>
2025-12-11 23:37:24 -0300

    hasta ejercicio 6

--- TrabajoPractico-MySQL.sql
+++ TrabajoPractico-MySQL.sql
@@ -1,6 +1,12 @@
 
+--Ejercicio 1  Crear Base de Datos
+--Crear una base de datos llamada veterinaria_patitas_felices.
+
+
 CREATE DATABASE veterinaria_patitas_felices ;
 USE veterinaria_patitas_felices;
+
+--Ejercicio 2  Crear tabla duenos
 
 CREATE TABLE duenos (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -9,3 +15,68 @@ CREATE TABLE duenos (
     telefono VARCHAR(20) NOT NULL,
     direccion VARCHAR(100)
 );
+
+CREATE TABLE  mascotas(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    especie VARCHAR(30) NOT NULL,
+    fecha_nacimiento DATE,
+    id_duenos INT,
+   FOREIGN KEY (id_duenos) REFERENCES duenos(id)
+);
+
+--Ejercicio 4 – Crear tabla veterinarios
+
+CREATE TABLE  veterinarios(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    matricula VARCHAR(20) UNIQUE NOT NULL,
+    especialidad VARCHAR (50) NOT NULL
+);
+
+--Ejercicio 5 – Crear tabla historial_clinico
+CREATE TABLE historial_clinico(
+id INT PRIMARY KEY AUTO_INCREMENT,
+id_mascota INT,
+id_veterinario INT,
+fecha_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+descripcion VARCHAR(250) NOT NULL,
+FOREIGN KEY (id_mascota) REFERENCES mascotas(id),
+FOREIGN KEY (id_veterinario) REFERENCES veterinarios(id)
+)
+
+--Ejercicio 6 – Insertar registros
+
+--insertar 3 dueños con información completa
+
+INSERT INTO duenos (nombre, apellido, direccion, telefono)
+VALUES
+('Laura', 'Gómez', 'Av. Belgrano 123, Buenos Aires', '1123456789'),
+('Martín', 'Pereira', 'Calle San Juan 456, Córdoba', '1139876543'),
+('Daniela', 'Torres', 'Ruta 8 km 32, San Isidro', '1145567788');
+
+ --insertar 3 mascotas, cada una asociada a un dueño
+
+INSERT INTO mascotas (nombre,especie,fecha_nacimiento,id_duenos)
+VALUES
+('Luna','perro','2019-05-10','1'),
+('Max','gato','2020-04-15','2'),
+('Nube','conejo','2022-03-05','3');
+
+--insertar 2 veterinarios con especialidades distintas
+
+INSERT INTO veterinarios(nombre,apellido,matricula,especialidad)
+VALUES
+('Sofia','Ramirez','VET12345','clinica general'),
+('Federico','Molina','VET67890','cirugia animal');
+
+--insertar 3 registros de historial clínico
+
+INSERT INTO historial_clinico(id_mascota,id_veterinario,descripcion)
+VALUES
+('1','2','Control general y vacunación anual'),
+('2','2','Consulta por pérdida de apetito; se indica análisis básico.'),
+('3','1','Revisión de rutina; todo dentro de parámetros normales.');
+
+
